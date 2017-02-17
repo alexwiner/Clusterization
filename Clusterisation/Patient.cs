bdfb1324aaf6290673c279d57903a3629bd57248
alexwiner <alexwiner@mail.ru>
2017-02-17 12:55:23 +0200

    new class patients

---
+++ Clusterisation/Patient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clusterisation
+{
+    class Patient
+    {
+        int Id;
+        List<double> Parameters;
+        int Cluster;
+        Patient(int id,List<double>parameters,int cluster)
+        {
+            Id = id;
+            Parameters = parameters;
+            Cluster = cluster;
+        }
+    }
+    
+}
